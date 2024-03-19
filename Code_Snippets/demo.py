0545b160ed72dd4e3e68603615d57f49971d6ca0
Dash_Presents <107453085+Dash619@users.noreply.github.com>
2024-03-19 10:12:08 -0400

    Optimize error handling and fix bugs
    Refined the code to simplify error handling mechanisms. Additionally, addressed and resolved minimal identified bugs, further improving application stability and performance.

--- Code_Snippets/demo.py
+++ Code_Snippets/demo.py
@@ -1,2 +1,10 @@
-letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g']
-print()
+if __name__ == '__main__':
+    n = int(input())
+    arr = input().split()
+
+    scores = [int(score) for score in arr]
+    max_score = max(scores)
+    while max_score in scores:
+        scores.remove(max_score)
+    runner_up_score = max(scores)
+    print(runner_up_score)
--- main.py
+++ main.py
@@ -2,40 +2,48 @@ while True:
     user_action = input("Type add, show, edit, complete or exit : ")
     user_action = user_action.strip()
 
-    if 'add' in user_action:
+    if user_action.startswith('add'):
         todo = user_action[4:] + '\n'
         with open('todos.txt', 'r') as file:
             todos = file.readlines()
             todos.append(todo)
         with open('todos.txt', 'w') as file:
             file.writelines(todos)
-    elif 'show' in user_action:
+    elif user_action.startswith('show'):
         with open('todos.txt', 'r') as file:
             todos = file.readlines()
         for index, item in enumerate(todos):
             item = item.strip('\n')
             row = f"{index + 1}-{item.capitalize()}"
             print(row)
-    elif 'edit' in user_action:
-        number = int(user_action[5:])
-        with open('todos.txt', 'r') as file:
-            todos = file.readlines()
-        new_todo = input("Enter a new todo : ")
-        todos[number - 1] = new_todo + '\n'
-        with open('todos.txt', 'w') as file:
-            file.writelines(todos)
-    elif 'complete' in user_action:
-        number = int(user_action[9:])
-        with open('todos.txt', 'r') as file:
-            todos = file.readlines()
-        index = number - 1
-        todo_to_remove = todos[index].strip('\n')
-        todos.pop(index)
-        with open('todos.txt', 'w') as file:
-            file.writelines(todos)
-        message = f"Todo {todo_to_remove} was removed from the list"
-        print(message)
-    elif 'exit' in user_action:
+    elif user_action.startswith('edit'):
+        try:
+            number = int(user_action[5:])
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
+            new_todo = input("Enter a new todo : ")
+            todos[number - 1] = new_todo + '\n'
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
+        except ValueError:
+            print("Command not valid!")
+            continue
+    elif user_action.startswith('complete'):
+        try:
+            number = int(user_action[9:])
+            with open('todos.txt', 'r') as file:
+                todos = file.readlines()
+            index = number - 1
+            todo_to_remove = todos[index].strip('\n')
+            todos.pop(index)
+            with open('todos.txt', 'w') as file:
+                file.writelines(todos)
+            message = f"Todo {todo_to_remove} was removed from the list"
+            print(message)
+        except IndexError:
+            print("No item with that number")
+            continue
+    elif user_action.startswith('exit'):
         break
     else:
         print("Command is not valid")
