d8f009c01934e2e06e5d78111a6bc4cf218baae5
Dash_Presents <107453085+Dash619@users.noreply.github.com>
2024-03-26 13:54:45 -0400

    Refactor code into modules and implement inter-module function calls
    Structured the codebase into modular components, adding specific functions within these modules to encapsulate related functionality. Enabled inter-module communication by calling these functions from other files, enhancing the code's organization and reusability.

--- main.py
+++ main.py
@@ -1,14 +1,4 @@
-def get_todos(filepath="todos.txt"):
-    """ Read a text file and return the list of to-do items """
-    with open(filepath, 'r') as file_local:
-        todos_local = file_local.readlines()
-    return todos_local
-
-
-def write_todos( todos_arg, filepath="todos.txt"):
-    """ Write the to-do items list in the text file"""
-    with open(filepath, 'w') as file_local:
-        file_local.writelines(todos)
+import functions
 
 
 while True:
@@ -17,11 +7,11 @@ while True:
 
     if user_action.startswith('add'):
         todo = user_action[4:] + '\n'
-        todos = get_todos()
+        todos = functions.get_todos()
         todos.append(todo)
-        write_todos(todos)
+        functions.write_todos(todos)
     elif user_action.startswith('show'):
-        todos = get_todos()
+        todos = functions.get_todos()
         for index, item in enumerate(todos):
             item = item.strip('\n')
             row = f"{index + 1}-{item.capitalize()}"
@@ -29,21 +19,21 @@ while True:
     elif user_action.startswith('edit'):
         try:
             number = int(user_action[5:])
-            todos = get_todos()
+            todos = functions.get_todos()
             new_todo = input("Enter a new todo : ")
             todos[number - 1] = new_todo + '\n'
-            write_todos(todos)
+            functions.write_todos(todos)
         except ValueError:
             print("Command not valid!")
             continue
     elif user_action.startswith('complete'):
         try:
             number = int(user_action[9:])
-            todos = get_todos()
+            todos = functions.get_todos()
             index = number - 1
             todo_to_remove = todos[index].strip('\n')
             todos.pop(index)
-            write_todos(todos)
+            functions.write_todos(todos)
             message = f"Todo {todo_to_remove} was removed from the list"
             print(message)
         except IndexError:
