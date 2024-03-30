2159528ee42debb83f611557dca72347651a83a9
Dash_Presents <107453085+Dash619@users.noreply.github.com>
2024-03-30 14:27:05 -0400

    Finalize supplementary measurement converter with GUI
    Completed the development of a supplementary measurement converter, equipped with a functional graphical user interface.

--- code_snippets/convertor.py
+++ code_snippets/convertor.py
@@ -1,24 +1,26 @@
-feet_inches = input("Enter feet and inches: ")
-
-
-def parse(feet_inches):
+def parse(feet_inches_local):
     parts = feet_inches.split(" ")
     feet = float(parts[0])
     inches = float(parts[1])
-    return {"feet":feet, "inches":inches}
+    return {"feet": feet, "inches": inches}
 
 
-def convert(feet,inches):
+def convert(feet, inches):
     meters = feet * 0.3048 + inches * 0.0254
     return meters
 
 
-parsed = parse(feet_inches)
-result = convert(parsed['feet'], parsed['inches'])
+if __name__ == "__main__":
 
-print(f"{parsed['feet']} feet and inches {parsed['inches']} is equal to {result} meters")
+    feet_inches = input("Enter feet and inches: ")
 
-if result < 1:
-    print("Kid is too small for ride")
-else:
-    print("Kid can use the slide")
+    parsed = parse(feet_inches)
+    result = convert(parsed['feet'], parsed['inches'])
+
+
+    print(f"{parsed['feet']} feet and inches {parsed['inches']} is equal to {result} meters")
+
+    if result < 1:
+        print("Kid is too small for ride")
+    else:
+        print("Kid can use the slide")
--- code_snippets/convertor_gui.py
+++ code_snippets/convertor_gui.py
@@ -1,18 +1,35 @@
 import PySimpleGUI as sg
+import convertor
+
 
 label_1 = sg.Text("Enter Feet : ")
-input_1 = sg.Input()
+input_1 = sg.Input(key="feet")
 
 label_2 = sg.Text("Enter Inches : ")
-input_2 = sg.Input()
+input_2 = sg.Input(key="inches")
 
 convert_button = sg.Button("Convert", tooltip='Convert to meters')
-
+output_label = sg.Text(key="meters", text_color="red")
 
 window = sg.Window('Convertor',
                    layout=[[label_1, input_1],
                            [label_2, input_2],
-                           [convert_button]])
+                           [convert_button, output_label]])
+while True:
+    event, values = window.read()
+    print(event, values)
+    match event:
+        case("Convert"):
+            feet = values['feet']
+            inches = values['inches']
+            print(float(feet))
+            print(float(inches))
+            meters = convertor.convert(float(feet), float(inches))
+            print(meters)
+            window['meters'].update(value=meters)
+        case sg.WIN_CLOSED:
+            break
+
 window.read()
 window.close()
 
