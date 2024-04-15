14a075704bd8852384ea8b4f5e062b31d577337c
Dash_Presents <107453085+Dash619@users.noreply.github.com>
2024-04-15 16:17:40 -0400

    Finalize program and add content to each page

--- animal_PDF.py
+++ animal_PDF.py
@@ -8,6 +8,10 @@ for filepath in filepaths:
     pdf.add_page()
     filename = Path(filepath).stem.capitalize()
     pdf.set_font(family="Times", size=16, style="B")
-    pdf.cell(w=50, h=8, txt=f"{filename}")
+    pdf.cell(w=50, h=8, txt=f"{filename}", ln=1)
+    with open(filepath, 'r') as file:
+        content = file.read()
+        pdf.set_font(family="Times", size=12)
+        pdf.multi_cell(w=0, h=6, txt=content)
 
-pdf.output(f"output.pdf")
+pdf.output(f"Animals/output.pdf")
